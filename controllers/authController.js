76dfa7d8d1deb689531e33ab622276f3457b4daa
abm6 <55770187+abm6@users.noreply.github.com>
2022-05-27 00:06:02 +0530

    add controller

---
+++ controllers/authController.js
@@ -0,0 +1,67 @@
+var axios = require('axios');
+
+const getUsers = () =>
+	new Promise((resolve, reject) => {
+		axios
+			.get('http://localhost:5000/users')
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+
+const addUser = async ({ username, password }) =>
+	new Promise((resolve, reject) => {
+		axios
+			.post('http://localhost:5000/users', { username, password })
+			.then((response) => {
+				console.log(response.data);
+				resolve('ok');
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+
+module.exports.signup_get = (req, res) => {
+	res.render('signup');
+};
+module.exports.signup_post = async (req, res) => {
+	const { username, password } = req.body;
+
+	let userExists = false;
+	await getUsers().then((users) => {
+		users.filter((user) => (user.username === username ? (userExists = true) : false));
+	});
+
+	if (userExists) res.sendStatus(400);
+	else
+		await addUser({ username, password })
+			.then((response) => {
+				console.log(response);
+				res.sendStatus(200);
+			})
+			.catch((error) => console.log(error));
+};
+
+module.exports.login_get = (req, res) => {
+	res.render('login');
+};
+
+
+module.exports.login_post = async (req, res) => {
+	const { username, password } = req.body;
+
+	let userExists = false;
+	await getUsers().then((users) => {
+		users.filter((user) => {
+      if (user.username === username && user.password === password) {
+        userExists = true;
+      }
+    });
+	});
+
+	userExists ? res.sendStatus(200) : res.sendStatus(400);
+};
