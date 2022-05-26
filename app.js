8ead6cea90829421ad160604cc4943c57ad6feec
abm6 <55770187+abm6@users.noreply.github.com>
2022-05-27 00:05:44 +0530

    express-gen

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const authRoutes = require('./routes/authRoutes');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 const app = express();
-const bcrypt = require('bcrypt');
-const PORT = 3000 | process.env.PORT;
-// const { getUsers } = require('./controller/userController');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var path = require('path');
 
-app.use(logger('dev'));
-// app.use(cookieParser());
-// app.use(session({ secret: 'loginsecret', resave: true, saveUninitialized: true, cookie: { secure: true } }));
-
-// all express configurations
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//routes
-
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.get('/', (req, res) => {
-	res.render('home');
+  res.render('home');
+});
+app.use(authRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
